3022bec29a479c37c08ab1f1ae0fe3358608d764
unknown <arabzadeh29880@gmail.com>
2024-12-22 15:19:53 +0330

    commit number3

--- lib/riverpod/riverpod_todo_list.dart
+++ lib/riverpod/riverpod_todo_list.dart
@@ -1,15 +1,38 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import "package:todo_app/models/todo.dart";
+
+
+enum WorkType { travel, work, playing, fun }
+
+class Todo {
+  final String id;
+  final String name;
+  final String description;
+  final DateTime date;
+  final WorkType workType;
+  final bool completed;
+
+  Todo({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.date,
+    required this.workType,
+    this.completed = false,
+  });
+}
 
 class TodoList extends StateNotifier<List<Todo>> {
-  TodoList() : super([]);
+  TodoList() : super([]); // the complete list of todos is empty initially but can be added later.
 
-  void add(String description) {
+  void add(String name, String description, DateTime date, WorkType workType) {
     state = [
       ...state,
       Todo(
         id: DateTime.now().toString(),
+        name: name,
         description: description,
+        date: date,
+        workType: workType,
       ),
     ];
   }
@@ -20,7 +43,10 @@ class TodoList extends StateNotifier<List<Todo>> {
         if (todo.id == id)
           Todo(
             id: todo.id,
+            name: todo.name,
             description: todo.description,
+            date: todo.date,
+            workType: todo.workType,
             completed: !todo.completed,
           )
         else
@@ -32,6 +58,7 @@ class TodoList extends StateNotifier<List<Todo>> {
     state = state.where((todo) => todo.id != id).toList();
   }
 }
+
 final todoListProvider = StateNotifierProvider<TodoList, List<Todo>>((ref) {
   return TodoList();
 });
--- lib/screens/add_todo_dialog.dart
+++ lib/screens/add_todo_dialog.dart
@@ -2,64 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/riverpod/riverpod_todo_list.dart';
 
-class TodoListScreen extends ConsumerWidget {
-  const TodoListScreen({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todoList = ref.watch(todoListProvider);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todo List'),
-      ),
-      body: ListView(
-        children: [
-          for (final todo in todoList)
-            ListTile(
-              title: Text(
-                todo.name,
-                style: const TextStyle(
-                    fontSize: 20, fontWeight: FontWeight.normal),
-              ),
-              subtitle: Text(
-                todo.description,
-                style:
-                    const TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
-              ),
-              leading: Checkbox(
-                value: todo.completed,
-                onChanged: (value) {
-                  ref.read(todoListProvider.notifier).toggle(todo.id);
-                },
-              ),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () {
-                  ref.read(todoListProvider.notifier).remove(todo.id);
-                },
-              ),
-            ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AddTodoDialog();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
 class AddTodoDialog extends ConsumerStatefulWidget {
-  const AddTodoDialog({super.key});
-
   @override
   _AddTodoDialogState createState() => _AddTodoDialogState();
 }
@@ -72,7 +15,6 @@ class _AddTodoDialogState extends ConsumerState<AddTodoDialog> {
 
   @override
   Widget build(BuildContext context) {
-    // AlertDialog is a dialog that interrupts the user's workflow to get a response from the user.
     return AlertDialog(
       title: const Text('Add Todo'),
       content: SingleChildScrollView(
@@ -80,22 +22,17 @@ class _AddTodoDialogState extends ConsumerState<AddTodoDialog> {
           children: [
             TextField(
               controller: _nameController,
-
-              decoration: const InputDecoration(
-                  hintText:
-                      'Enter name'), // InputDecoration is used to configure the visual appearance of an input field.
+              decoration: InputDecoration(hintText: 'Enter name'),
             ),
             TextField(
               controller: _taskController,
-              decoration:
-                  const InputDecoration(hintText: 'Enter task description'),
+              decoration: InputDecoration(hintText: 'Enter task description'),
             ),
             ListTile(
               title: Text(_selectedDate == null
                   ? 'Select date'
-                  : 'Date: ${_selectedDate!.toLocal()}'.split(' ')[
-                      0]), // toLocal() converts the date to the local time zone and split(' ')[0] is used to get the date only.
-              trailing: const Icon(Icons.calendar_today),
+                  : 'Date: ${_selectedDate!.toLocal()}'.split(' ')[0]),
+              trailing: Icon(Icons.calendar_today),
               onTap: () async {
                 DateTime? picked = await showDatePicker(
                   context: context,
@@ -103,15 +40,14 @@ class _AddTodoDialogState extends ConsumerState<AddTodoDialog> {
                   firstDate: DateTime(2000),
                   lastDate: DateTime(2101),
                 );
-                if (picked != null && picked != _selectedDate) {
+                if (picked != null && picked != _selectedDate)
                   setState(() {
                     _selectedDate = picked;
                   });
-                }
               },
             ),
             DropdownButton<WorkType>(
-              hint: const Text('Select work type'),
+              hint: Text('Select work type'),
               value: _selectedWorkType,
               onChanged: (WorkType? newValue) {
                 setState(() {
